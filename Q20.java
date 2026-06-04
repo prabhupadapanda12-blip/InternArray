fd0f024456b752ba087b13e9f951528b314a4872
prabhupadapanda12-blip <prabhupadapanda74@gmail.com>
2026-06-04 23:47:14 +0530

    InternArray

---
+++ Q20.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Q20 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        int sum = 0;
+
+        for(int i=0;i<n;i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i=0;i<n;i++) {
+            sum = sum + arr[i];
+        }
+
+        double avg = (double)sum / n;
+
+        System.out.println("Average = " + avg);
+    }
+}
